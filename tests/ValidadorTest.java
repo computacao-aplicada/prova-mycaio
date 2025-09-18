21bde609b4c2e0c615aba864cafefba1211e4a32
mycaio <caio.mendonca@edu.unifil.br>
2025-09-17 21:29:33 -0300

    subindo teste cpf valido(11 digitos/ com .- /)

--- tests/ValidadorTest.java
+++ tests/ValidadorTest.java
@@ -1,8 +1,11 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class ValidadorTest {
 
-public class ValidadorTest {
-
+    @Test
+    void deveValidarCPFValido() {
+        assertTrue(Validador.validarCPF("529.982.247-25"));
+        assertTrue(Validador.validarCPF("52998224725"));
+    }
 }
